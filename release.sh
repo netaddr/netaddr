4a0da8afcecf6947e6087687a650d3e15dbcfbae
David P. D. Moss <drkjam@gmail.com>
2009-03-31 00:32:15 +0000

    Added auto-generation for epydocs to release script.

--- release.sh
+++ release.sh
@@ -6,6 +6,10 @@ cd $(dirname $0)
 rm docs/api/*
 #   epydoc is required below - http://epydoc.sourceforge.net/
 epydoc --config=docs/epydoc.cfg
+cd docs/api/
+zip ../netaddr.zip *
+echo "upload your docs to PyPI with the release"
+cd ../..
 python setup.py sdist --no-defaults --formats=gztar,zip --dist-dir=../builds/
 #   egg setup
 python setup_egg.py bdist_egg --dist-dir=../builds/
