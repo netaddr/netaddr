d5d7c5faecedc0ca9f4b16460d39f43c30119753
David P. D. Moss <drkjam@gmail.com>
2010-04-28 22:32:51 +0000

    Updated all copyright notices for 2010.

--- lib3/netaddr/strategy/__init__.py
+++ lib3/netaddr/strategy/__init__.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-#   Copyright (c) 2008-2009, David P. D. Moss. All rights reserved.
+#   Copyright (c) 2008-2010, David P. D. Moss. All rights reserved.
 #
 #   Released under the BSD license. See the LICENSE file for details.
 #-----------------------------------------------------------------------------
--- lib3/netaddr/tests/netaddr_coverage.sh
+++ lib3/netaddr/tests/netaddr_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------
-#   Copyright (c) 2008-2009, David P. D. Moss. All rights reserved.
+#   Copyright (c) 2008-2010, David P. D. Moss. All rights reserved.
 #
 #   Checks unit test code coverage for the netaddr project.
 #-----------------------------------------------------------------------------
