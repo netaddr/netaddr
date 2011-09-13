d7abad7280ca89eff43bb9790130d7b8e07ae9c7
David Moss <drkjam@gmail.com>
2011-09-14 00:13:46 +0100

    - added trove classifier for Python 3.2

--- release.py
+++ release.py
@@ -113,6 +113,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.0',
     'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
     'Topic :: Communications',
     'Topic :: Documentation',
     'Topic :: Education',
