2d69f6f1491605712775a2cd8a6ee444b573260d
David Moss <drkjam@gmail.com>
2012-05-24 22:09:40 +0100

    - updated copyright notice for 2012

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 :-----------------------------------------------------------------------------
-:   Copyright (c) 2008-2011, David P. D. Moss. All rights reserved.
+:   Copyright (c) 2008-2012, David P. D. Moss. All rights reserved.
 :
 :   Released under the BSD license. See the LICENSE file for details.
 :-----------------------------------------------------------------------------
