98db2b9e7b8331af422b32187732a64dcd1d66e6
nordhausen <stefan.nordhausen@axiros.com>
2014-04-11 13:48:42 +0200

    Add IPSet.iter_ipranges()

--- netaddr/ip/sets.py
+++ netaddr/ip/sets.py
@@ -741,3 +741,16 @@ class IPSet(object):
             return IPRange(cidrs[0][0], cidrs[-1][-1])
         else:
             raise ValueError("IPSet is not contiguous")
+
+    def iter_ipranges(self):
+        """Generate the merged IPRanges for this IPSet.
+
+        In contrast to self.iprange(), this will work even when the IPSet is
+        not contiguous. Adjacent IPRanges will be merged together, so you
+        get the minimal number of IPRanges.
+        """
+        sorted_ranges = [(cidr._module.version, cidr.first, cidr.last) for
+                cidr in self.iter_cidrs()]
+
+        for start, stop in _iter_merged_ranges(sorted_ranges):
+            yield IPRange(start, stop)
