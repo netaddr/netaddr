960c06f46efa4bbee806b8f825384868b24c9880
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-07-22 10:55:14 +0200

    Issue 96: Do not change behavior for IPv6

--- test/ip/test_ip_v4.py
+++ test/ip/test_ip_v4.py
@@ -499,3 +499,7 @@ def test_rfc3021_subnets():
     assert IPNetwork('192.0.2.0/32').network == IPAddress('192.0.2.0')
     assert IPNetwork('192.0.2.0/32').broadcast is None
     assert list(IPNetwork('192.0.2.0/32').iter_hosts()) == [IPAddress('192.0.2.0')]
+
+    # IPv6 must not be affected
+    assert IPNetwork('abcd::/127').broadcast is not None
+    assert IPNetwork('abcd::/128').broadcast is not None
