fdd126f68a5e471f8b2c64a9a1791e44becbb900
David Moss <drkjam@gmail.com>
2016-09-17 21:58:00 +0100

    - bumping version for upcoming release and removing __all__

--- netaddr/__init__.py
+++ netaddr/__init__.py
@@ -6,8 +6,8 @@
 """A Python library for manipulating IP and EUI network addresses."""
 
 #: Version info (major, minor, maintenance, status)
-VERSION = (0, 7, 18)
-STATUS = ''
+VERSION = (0, 7, 19)
+STATUS = '-dev'
 __version__ = '%d.%d.%d' % VERSION[0:3] + STATUS
 
 import sys as _sys
@@ -44,45 +44,3 @@ from netaddr.strategy.eui48 import (mac_eui48, mac_unix, mac_unix_expanded,
 
 from netaddr.strategy.eui64 import (eui64_base, eui64_unix, eui64_unix_expanded,
         eui64_cisco, eui64_bare, valid_str as valid_eui64)
-
-__all__ = [
-    #   Constants.
-    'ZEROFILL', 'Z', 'INET_PTON', 'P', 'NOHOST', 'N',
-
-    #   Custom Exceptions.
-    'AddrConversionError', 'AddrFormatError', 'NotRegisteredError',
-
-    #   IP classes.
-    'IPAddress', 'IPNetwork', 'IPRange', 'IPSet',
-
-    #   IPv6 dialect classes.
-    'ipv6_compact', 'ipv6_full', 'ipv6_verbose',
-
-    #   IP functions and generators.
-    'all_matching_cidrs', 'cidr_abbrev_to_verbose', 'cidr_exclude',
-    'cidr_merge', 'iprange_to_cidrs', 'iter_iprange', 'iter_unique_ips',
-    'largest_matching_cidr', 'smallest_matching_cidr', 'spanning_cidr',
-
-    #   IP globbing class.
-    'IPGlob',
-
-    #   IP globbing functions.
-    'cidr_to_glob', 'glob_to_cidrs', 'glob_to_iprange', 'glob_to_iptuple',
-    'iprange_to_globs',
-
-    #   IEEE EUI classes.
-    'EUI', 'IAB', 'OUI',
-
-    #   EUI-48 (MAC) dialect classes.
-    'mac_bare', 'mac_cisco', 'mac_eui48', 'mac_pgsql', 'mac_unix',
-    'mac_unix_expanded',
-
-    #   Validation functions.
-    'valid_ipv4', 'valid_ipv6', 'valid_glob', 'valid_mac',
-
-    #   nmap-style range functions.
-    'valid_nmap_range', 'iter_nmap_range',
-
-    #   RFC 1924 functions.
-    'base85_to_ipv6', 'ipv6_to_base85',
-]
