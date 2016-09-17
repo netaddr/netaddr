f33cabf3bb43ad216514b4351f2d220fc3a905dc
David Moss <drkjam@gmail.com>
2016-09-17 22:20:42 +0100

    - added extra IPAddress.netmask_bits tests

--- netaddr/tests/ip/test_ip_v4.py
+++ netaddr/tests/ip/test_ip_v4.py
@@ -400,10 +400,15 @@ def test_ipnetwork_bad_string_constructor():
 
 
 def test_ipaddress_netmask_v4():
-    assert IPAddress('1.1.1.1').netmask_bits() == 32
-    assert IPAddress('255.255.255.254').netmask_bits() == 31
-    assert IPAddress('255.255.255.0').netmask_bits() == 24
     assert IPAddress('0.0.0.0').netmask_bits() == 0
+    assert IPAddress('128.0.0.0').netmask_bits() == 1
+    assert IPAddress('255.0.0.0').netmask_bits() == 8
+    assert IPAddress('255.255.0.0').netmask_bits() == 16
+    assert IPAddress('255.255.255.0').netmask_bits() == 24
+    assert IPAddress('255.255.255.254').netmask_bits() == 31
+    assert IPAddress('255.255.255.255').netmask_bits() == 32
+
+    assert IPAddress('1.1.1.1').netmask_bits() == 32
 
 
 def test_ipaddress_hex_format():
--- netaddr/tests/ip/test_ip_v6.py
+++ netaddr/tests/ip/test_ip_v6.py
@@ -85,6 +85,13 @@ def test_ipnetwork_constructor_v6():
 
 def test_ipaddress_netmask_v6():
     assert IPAddress('::').netmask_bits() == 0
+    assert IPAddress('8000::').netmask_bits() == 1
+    assert IPAddress('ffff:ffff:ffff:ffff::').netmask_bits() == 64
+    assert IPAddress('ffff:ffff:ffff:ffff:ffff:ffff:ffff::').netmask_bits() == 112
+    assert IPAddress('ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffe').netmask_bits() == 127
+    assert IPAddress('ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff').netmask_bits() == 128
+
+    assert IPAddress('fe80::1').netmask_bits() == 128
 
 
 def test_objects_use_slots():
