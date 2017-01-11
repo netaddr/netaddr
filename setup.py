a3094fa69c92471736a12ac2912c8768dd907785
David Moss <drkjam@gmail.com>
2017-01-11 23:43:33 +0000

    - test suite passed under Python 3.6

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ classifiers = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Communications',
     'Topic :: Documentation',
     'Topic :: Education',
