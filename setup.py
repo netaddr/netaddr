583b5627c306cad8bebcd4df5ad17a4f3e436fc7
Jakub Stasiak <jakub@stasiak.at>
2020-06-19 01:23:18 +0200

    Fix description formatting, PyPI enforces ReST now

--- setup.py
+++ setup.py
@@ -42,45 +42,45 @@ package_data = {
 #   NB - keep this text around 74 characters wide so it is viewable
 #        in various fixed window sizes.
 long_description = """
-        Provides support for:
+Provides support for:
 
-        Layer 3 addresses
-        -----------------
+Layer 3 addresses
+-----------------
 
-        - IPv4 and IPv6 addresses, subnets, masks, prefixes
-        - iterating, slicing, sorting, summarizing and classifying IP networks
-        - dealing with various ranges formats (CIDR, arbitrary ranges and globs, nmap)
-        - set based operations (unions, intersections etc) over IP addresses and subnets
-        - parsing a large variety of different formats and notations
-        - looking up IANA IP block information
-        - generating DNS reverse lookups
-        - supernetting and subnetting
+- IPv4 and IPv6 addresses, subnets, masks, prefixes
+- iterating, slicing, sorting, summarizing and classifying IP networks
+- dealing with various ranges formats (CIDR, arbitrary ranges and globs, nmap)
+- set based operations (unions, intersections etc) over IP addresses and subnets
+- parsing a large variety of different formats and notations
+- looking up IANA IP block information
+- generating DNS reverse lookups
+- supernetting and subnetting
 
-        Layer 2 addresses
-        -----------------
+Layer 2 addresses
+-----------------
 
-        - representation and manipulation MAC addresses and EUI-64 identifiers
-        - looking up IEEE organisational information (OUI, IAB)
-        - generating derived IPv6 addresses
+- representation and manipulation MAC addresses and EUI-64 identifiers
+- looking up IEEE organisational information (OUI, IAB)
+- generating derived IPv6 addresses
 
-        Documentation
-        -------------
+Documentation
+-------------
 
-        https://netaddr.readthedocs.io/
+https://netaddr.readthedocs.io/
 
-        Changes
-        -------
+Changes
+-------
 
-        For details on the latest changes and updates, see :-
+For details on the latest changes and updates, see :-
 
-        https://netaddr.readthedocs.io/en/latest/changes.html
+https://netaddr.readthedocs.io/en/latest/changes.html
 
-        Requirements
-        ------------
+Requirements
+------------
 
-        Supports Python versions 2.5 through 3.6
+Supports Python versions 2.5 through 3.6
 
-        Share and enjoy!
+Share and enjoy!
 """
 
 platforms = 'OS Independent'
