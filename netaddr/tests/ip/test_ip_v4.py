8cb875a7992181dba653148044bac9cc508dc1e2
Jakub Stasiak <jakub@stasiak.at>
2020-06-21 00:55:14 +0200

    Verify bytes(IPAddress) behavior

--- netaddr/tests/ip/test_ip_v4.py
+++ netaddr/tests/ip/test_ip_v4.py
@@ -15,6 +15,7 @@ def test_ipaddress_v4():
     assert ip.format() == '192.0.2.1'
     assert int(ip) == 3221225985
     assert hex(ip) == '0xc0000201'
+    assert bytes(ip) == b'\xc0\x00\x02\x01'
     assert ip.bin == '0b11000000000000000000001000000001'
     assert ip.bits() == '11000000.00000000.00000010.00000001'
     assert ip.words == (192, 0, 2, 1)
