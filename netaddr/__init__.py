0d5318c21c7af682c842e01177fee7247416b9a3
Jakub Stasiak <jakub@stasiak.at>
2020-07-03 16:26:11 +0200

    Prepare release 0.8.0

--- netaddr/__init__.py
+++ netaddr/__init__.py
@@ -6,7 +6,7 @@
 """A Python library for manipulating IP and EUI network addresses."""
 
 #: Version info (major, minor, maintenance, status)
-__version__ = '0.7.20'
+__version__ = '0.8.0'
 VERSION = tuple(int(part) for part in __version__.split('.'))
 STATUS = ''
 
