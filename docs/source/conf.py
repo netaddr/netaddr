bb543157f9fd05c8575821536bd5b5b3ea43711e
Jakub Stasiak <jakub@stasiak.at>
2023-12-21 02:23:24 +0100

    Style external links (#291)
    I sometimes find it useful to see if a link takes me to "the outside" or
    if I'm still gonna be in the documentation when I click it.
    
    The style snippet taken almost verbatim from [1].
    
    [1] https://github.com/pradyunsg/furo/discussions/348#discussioncomment-1931019

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -120,7 +120,7 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -254,3 +254,6 @@ nitpicky = True
 autodoc_default_options = {
     'special-members': '__init__',
 }
+
+
+html_css_files = ['custom.css']
