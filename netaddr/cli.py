2e4a3b439924164dd78f32a2cd15823fdfa2b20f
Jakub Stasiak <jakub@stasiak.at>
2024-02-10 02:06:04 +0100

    Make the shell banner nicer

--- netaddr/cli.py
+++ netaddr/cli.py
@@ -19,7 +19,14 @@ from netaddr import EUI as MAC
 def main():
     argv = sys.argv[1:]
 
-    banner = '\nnetaddr shell %s - %s\n' % (netaddr.__version__, __doc__)
+    banner = r"""               __            __    __
+   ____  ___  / /_____ _____/ /___/ /____
+  / __ \/ _ \/ __/ __ `/ __  / __  / ___/
+ / / / /  __/ /_/ /_/ / /_/ / /_/ / /
+/_/ /_/\___/\__/\__,_/\__,_/\__,_/_/
+
+netaddr shell %s - %s
+""" % (netaddr.__version__, __doc__)
     exit_msg = '\nShare and enjoy!'
     rc_override = None
 
