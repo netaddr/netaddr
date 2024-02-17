9725e1f32502470a665a53ef56a764c3f803226d
Jakub Stasiak <jakub@stasiak.at>
2024-02-17 09:16:06 +0100

    Improve the exceptions raised by fbsocket's inet_pton
    The original socket.inet_pton raises TypeError on non-str passed as an
    address and OSError in case of an invalid address.
    
    This will help with more precise exception handling (we use
    socket.inet_pton and fbsocket.inet_pton interchangeably in a couple of
    places).

--- netaddr/tests/ip/test_socket_module_fallback.py
+++ netaddr/tests/ip/test_socket_module_fallback.py
@@ -1,6 +1,6 @@
 import pytest
 
-from netaddr.fbsocket import inet_ntop, inet_pton, inet_ntoa, AF_INET6
+from netaddr.fbsocket import inet_ntop, inet_pton, inet_ntoa, AF_INET, AF_INET6
 
 
 @pytest.mark.parametrize(
@@ -35,6 +35,17 @@ def test_inet_ntoa_ipv4_exceptions():
         inet_ntoa('\x00')
 
 
+def test_inet_pton_ipv4_exceptions():
+    with pytest.raises(OSError):
+        inet_pton(AF_INET, '::0x07f')
+
+    with pytest.raises(TypeError):
+        inet_pton(AF_INET, 1)
+
+
 def test_inet_pton_ipv6_exceptions():
-    with pytest.raises(ValueError):
+    with pytest.raises(OSError):
         inet_pton(AF_INET6, '::0x07f')
+
+    with pytest.raises(TypeError):
+        inet_pton(AF_INET6, 1)
