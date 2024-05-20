netaddr/netaddr

commit d39b7d12428006b2ca1e260f92da2b66948b0d6d

Handle IP parsing errors more precisely
Reducing the amount of code in try blocks and catching only specific
exception types will both make the code easier to understand but also
stop hiding problems (see: IPv6 valid_str hiding TypeErrors before).

netaddr/strategy/ipv4.py
@@ -121,11 +121,12 @@ def str_to_int(addr, flags=0):
 
     try:
         if pton_mode:
-            return _struct.unpack('>I', _inet_pton(AF_INET, addr))[0]
+            packed = _inet_pton(AF_INET, addr)
         else:
-            return _struct.unpack('>I', _inet_aton(addr))[0]
-    except Exception:
+            packed = _inet_aton(addr)
+    except OSError:
         raise error
+    return _struct.unpack('>I', packed)[0]
 
 
 def int_to_str(int_val, dialect=None):

netaddr/ip/__init__.py
@@ -331,7 +331,7 @@ class IPAddress(BaseIP):
                     for module in _ipv4, _ipv6:
                         try:
                             self._value = module.str_to_int(addr, flags)
-                        except:
+                        except AddrFormatError:
                             continue
                         else:
                             self._module = module

netaddr/strategy/ipv6.py
@@ -126,7 +126,7 @@ def valid_str(addr, flags=0):
     """
     try:
         _inet_pton(AF_INET6, addr)
-    except:
+    except OSError:
         return False
     return True
 
@@ -142,9 +142,9 @@ def str_to_int(addr, flags=0):
     """
     try:
         packed_int = _inet_pton(AF_INET6, addr)
-        return packed_to_int(packed_int)
-    except Exception:
+    except OSError:
         raise AddrFormatError('%r is not a valid IPv6 address string!' % (addr,))
+    return packed_to_int(packed_int)
 
 
 def int_to_str(int_val, dialect=None):

netaddr/tests/strategy/test_ipv6_strategy.py
@@ -87,12 +87,6 @@ def test_strategy_ipv6_valid_str(str_value):
         '',
         'g:h:i:j:k:l:m:n',  # bad chars.
         '0:0:0:0:0:0:0:0:0',  # too long,
-        #   Unexpected types.
-        [],
-        (),
-        {},
-        True,
-        False,
     ),
 )
 def test_strategy_ipv6_is_not_valid_str(str_value):

commit 7e66eed681d86c445017b61a35d7be67668c2c92

Fix IPv6 parsing with ZEROFILL enabled
It was a regression, affected IPAddress.

Fixes: d39b7d124280 ("Handle IP parsing errors more precisely")
Resolves: https://github.com/netaddr/netaddr/issues/383

netaddr/strategy/ipv4.py
@@ -115,6 +115,8 @@ def str_to_int(addr, flags=0):
     :return: The equivalent unsigned integer for a given IPv4 address.
     """
     error = AddrFormatError('%r is not a valid IPv4 address string!' % (addr,))
+    if ':' in addr:
+        raise error
     pton_mode = flags & INET_PTON or not flags & INET_ATON
     if flags & ZEROFILL:
         addr = '.'.join(['%d' % int(i) for i in addr.split('.')])

netaddr/tests/ip/test_ip_v6.py
@@ -1,6 +1,6 @@
 import pickle
 import pytest
-from netaddr import IPAddress, IPNetwork
+from netaddr import IPAddress, IPNetwork, ZEROFILL
 
 
 def test_ipaddress_v6():
@@ -174,3 +174,7 @@ def test_rfc6164_subnets():
     assert IPNetwork('1234::/128').broadcast is None
     assert list(IPNetwork('1234::/128')) == [IPAddress('1234::')]
     assert list(IPNetwork('1234::/128').iter_hosts()) == [IPAddress('1234::')]
+
+
+def test_ipaddress_ignores_zerofill_when_parsing_ipv6():
+    assert IPAddress('fe80::', flags=ZEROFILL)

netaddr/tests/strategy/test_ipv4_strategy.py
@@ -1,6 +1,6 @@
 import pytest
 
-from netaddr import INET_ATON, INET_PTON, AddrFormatError
+from netaddr import INET_ATON, INET_PTON, AddrFormatError, ZEROFILL
 from netaddr.strategy import ipv4
 
 
@@ -37,6 +37,11 @@ def test_strategy_inet_aton_behaviour():
     assert ipv4.str_to_int('0x7f.1', flags=INET_ATON) == 2130706433
     assert ipv4.str_to_int('0177.1', flags=INET_ATON) == 2130706433
     assert ipv4.str_to_int('127.0.0.1', flags=INET_ATON) == 2130706433
+
+
+def test_str_to_int_correctly_rejects_ipv6_with_zerofill():
+    with pytest.raises(AddrFormatError):
+        ipv4.str_to_int('fe80::', flags=ZEROFILL)
 
 
 def test_strategy_inet_pton_behaviour():